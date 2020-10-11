5019f5ab19a72da5c2e76746f220bc4bae708a84
Nazha <43904074+Shirousaber@users.noreply.github.com>
2020-10-11 19:20:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const koaBody = require('koa-body');
+app.use(koaBody({
+    multipart: true,
+    formidable: {
+        maxFileSize: 200*1024*1024    // 设置上传文件大小最大限制，默认2M
+    }
+}));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
