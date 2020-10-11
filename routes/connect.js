fa2b0f7f069bd560b7fb835648a3bb0a694da450
Nazha <43904074+Shirousaber@users.noreply.github.com>
2020-10-11 19:57:48 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -148,9 +148,12 @@ router.get('/dload',(req, res)=>{
 })
 
 
-router.post('/file_upload',function(req, res){
-	var des_file = __dirname + "/" + req.files[0].originalname;
-	fs.readFile( req.files[0].path, function (err, data) {  // 异步读取文件内容
+router.post('/file_upload', function (req, res) {
+ 
+   console.log(req.files[0]);  // 上传的文件信息
+ 
+   var des_file = __dirname + "/" + req.files[0].originalname; //文件名
+   fs.readFile( req.files[0].path, function (err, data) {  // 异步读取文件内容
         fs.writeFile(des_file, data, function (err) { // des_file是文件名，data，文件数据，异步写入到文件
          if( err ){
               console.log( err );
@@ -165,8 +168,7 @@ router.post('/file_upload',function(req, res){
           res.end( JSON.stringify( response ) );
        });
    });
-}
-);
+})
 
 module.exports=router;
 
