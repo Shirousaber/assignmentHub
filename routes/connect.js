c4784880b2514c960b8ddd082ddb011adb031aa2
Nazha <43904074+Shirousaber@users.noreply.github.com>
2020-10-11 16:46:24 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -136,5 +136,33 @@ router.get('/dload',(req, res)=>{
     
 })
 
+var upload = multer({ dest: '/tmp/' })
+router.post('/file_upload', upload.array('image'), function(req, res, next) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+    
+    if(undefined == req.files[0]){
+        res.json(['failed', {msg:"没有选择要上传的文件！"}]);
+        return -1;
+    }
+
+    var des_file = "./files/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+                res.json(['failed', {msg:err}]);
+            }else{
+                response = {
+                    msg:'File uploaded successfully', 
+                    filename:req.files[0].originalname,
+                };
+                console.log( response );
+                res.json(['success', response]);
+            }
+        });
+    });
+});
+
 module.exports=router;
 
