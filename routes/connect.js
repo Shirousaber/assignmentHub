436a5a1cb1d9b29788d91d4c4227cb169f0cde71
Nazha <43904074+Shirousaber@users.noreply.github.com>
2020-10-17 11:22:46 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -44,7 +44,7 @@ router.post('/file_upload', function (req, res) {
    var myla = myid.split(".")[1];
    console.log(mysid);
    console.log(myid);
-   var des_file ="/var/ftp/pub/" +myid+"/"+mysid+".pdf"; //文件名
+   var des_file ="/var/ftp/pub/" +myid+"/"+mysid+"."+myla; //文件名
    console.log('des_file'+des_file);
 //    if(!fs.existsSync("/var/ftp/pub/" +myid)){
 // 	    fs.mkdir("/var/ftp/pub/" +myid);
