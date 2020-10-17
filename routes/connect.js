43f0b19a0bf56bc470b6dacfcd9a86fc305a195e
Nazha <43904074+Shirousaber@users.noreply.github.com>
2020-10-17 10:23:13 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const ftp = require('ftp');//连接FTP
 const path = require('path');
-//const client = new ftp();
 const fs = require('fs');
 const multer = require('multer');
 const koaBody = require('koa-body');
@@ -22,75 +21,23 @@ router.get('/',(req, res)=>{
     res.render('upload');
     
 })
-// function ftplog(){
-//     client.on('ready',()=>{
-//     console.log(new Date());
-//     console.log('ftp client is ready');
-//     });
-//     client.on('close',()=>{
-//     console.log('ftp client has close')
-//     });
-//     client.on('end',()=>{
-//     console.log('ftp client has end')
-//     });
-//     client.on('error',(err)=>{
-//     console.log('ftp client has an error : '+ JSON.stringify(err))
-//     });
-// }
+
 
 router.get('/upload',(req, res)=>{
     var id = req.query.id;
     var curuser = req.query.user;
     const client = new ftp();
-//     multer({
-//         dest:'/var/ftp/pub/'+id+'/'
-//     }).single('D:/下载/'+curuser+'_'+id+'.doc'),(req,res)=>{
-//         console.log('OKK');
-//         res.send(req.file);
-//     }
-//     if (!fs.existsSync('/var/ftp/pub/'+id+'/')) {
-//      fs.mkdirSync('/var/ftp/pub/'+id+'/');
-//     }
-//     var stream = fs.createWriteStream('/var/ftp/pub/'+id+'/'+ curuser+'.doc');
-//     request('D:/下载/'+curuser+'_'+id+'.doc').pipe(stream).on('close', function(err){
-//         if(err){
-//             throw err;
-//         }else{
-//         }
-//     });
-    //console.log(id);
-    //console.log(target);
-    //日志输出
-    // //切换目录
-    // function cwd(dirpath){
-    //     return new Promise((resolve,reject)=>{
-    //     client.cwd(dirpath,(err,dir)=>{
-    //     resolve({err : err,dir : dir});
-    //     })
-    //     });
-    // }
-
-    // //将文件上传到ftp目标地址
-    // async function put(currentFile,targetFilePath){
-    //     const dirpath = path.dirname(targetFilePath);
-    //     const fileName = path.basename(targetFilePath);
-    //     const rs = fs.createReadStream(currentFile);
-    //     let {err : ea,dir} = await cwd(dirpath);//此处应对err做处理
-    //     if(ea){
-    //     return Promise.resolve({err : ea});
-    //     }
-    //     return new Promise((resolve,reject)=>{
-    //     client.put(rs,fileName,(err)=>{
-    //     resolve({err : err});
-    //     })
-    //     });
-    // }
+    //如果不存在该路径就创建相关文件夹
+    if(!fs.existsSync('/var/ftp/pub/'+id)){
+	    fs.mkdir('/var/ftp/pub/'+id);
+    }
     //新宝岛上传
     fs.readFile('http://'+req.headers.host+'/D:/上传/' +curuser+'_'+id+'.txt', function (err, data) {
 		fs.writeFile('/var/ftp/pub/'+id+'/'+curuser+'_'+id+'.remote-copy.txt', data, function (err) {
 			console.log(data)
 			if( err ){
 				console.log( err );
+				res.send('文件上传失败');
 			}else{
 				console.log('文件上传成功');
 				console.log(new Date());
@@ -105,19 +52,7 @@ router.get('/upload',(req, res)=>{
 		});
 	});
     
-//         client.on('ready', function(){
-//             client.put('D:/上传/'+curuser+'_'+id+'.txt','pub/'+id+'/'+curuser+'_'+id+'.remote-copy.txt',function(err){
-//                 if(err){
-//                     res.send('ftp异常！');
-//                 }else{
-//                     console.log('文件上传成功')
-//                     console.log(new Date());
-//                     client.end();
-//                 } 
-//             });
-//             })
-//         //文件上传
-//         client.connect(ftpTarget);
+
         
 })
 //文件下载
@@ -134,32 +69,6 @@ router.get('/dload',(req, res)=>{
 		    console.log('文件下载成功');
 		    console.log(new Date());
 	    }
-//         if(err)
-//         {
-//             throw err;
-//         }else{
-//         }
-    })
-//     var id = req.query.id;
-//     console.log(id)
-//     //日志输出
-//     client.on('ready',()=>{
-//     console.log(new Date());
-//     console.log('ftp client is ready');
-//     });
-//     client.on('close',()=>{
-//     console.log('ftp client has close')
-//     });
-//     client.on('end',()=>{
-//     console.log('ftp client has end')
-//     });
-//     client.on('error',(err)=>{
-//     console.log('ftp client has an error : '+ JSON.stringify(err))
-//     });
-
-
-
-//     client.connect(ftpTarget);
 
 
     
@@ -176,6 +85,9 @@ router.post('/file_upload', function (req, res) {
    console.log(myid);
    var des_file ="/var/ftp/pub/" +myid+"/"+mysid+".zip"; //文件名
    console.log(des_file);
+   if(!fs.existsSync("/var/ftp/pub/" +myid)){
+	    fs.mkdir("/var/ftp/pub/" +myid);
+    }
    fs.readFile( req.files[0].path, function (err, data) {  // 异步读取文件内容
         fs.writeFile(des_file, data, function (err) { // des_file是文件名，data，文件数据，异步写入到文件
          if( err ){
