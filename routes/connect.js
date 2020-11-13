454cff41551b55eaf632bbf976162b02ba13e0ef
MS-05 <43904074+Shirousaber@users.noreply.github.com>
2020-11-13 23:41:54 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -16,7 +16,7 @@ router.get('/',(req, res)=>{
 })
 
 router.get('/test_count_doc',(req, res)=>{
-    var raws = mammoth.extractRawText({path:"/var/ftp/pub/test.docx"}).value;
+    var raws = mammoth.extractRawText({path:"/var/ftp/pub/测试.docx"}).value;
     console.log(raws);
 //     console.log(mammoth.extractRawText(file_ad).value.length);
     res.render('upload');
