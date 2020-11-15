750434969c226eeff946a71e1aee83fbd0f42a9f
MS-05 <43904074+Shirousaber@users.noreply.github.com>
2020-11-15 12:21:47 +0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -162,10 +162,10 @@ router.get('/tclogin',function(req,res){
 //                 }
 //                 sqlConnect(sql,sqlArr,callBack)      
 //              res.sendfile(path.join(__dirname,"../public/tIndex.html"))
-            res.render = {'tIndex',{
+            res.render = ('tIndex',{
                           curruser:curruser
                          }
-            }
+            )
             //res.sendfile(path.join(__dirname,"../public/teacherd.html"))
         }
     })
