e75ba34874f32e072a18645e5badfc6c119c3f31
MS-05 <43904074+Shirousaber@users.noreply.github.com>
2020-11-16 20:44:07 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -158,7 +158,7 @@ router.post('/file_upload', function (req, res) {
                   filename: req.files[0].originalname,
                   count: my_cnt
                 };
-                SQLupdate(10, my_cnt);
+                SQLupdate(1, my_cnt);
                 console.log(response);
                 res.end(JSON.stringify(response));
               });
