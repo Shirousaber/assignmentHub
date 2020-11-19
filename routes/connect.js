72c26ba370dbd64eb7ae0a0debcd471ba1f55aec
MS-05 <43904074+Shirousaber@users.noreply.github.com>
2020-11-19 19:36:07 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -216,7 +216,7 @@ router.post('/file_upload', function (req, res) {
                   }
                 });
 
-                cp.exec("echo |wc -c /var/ftp/pub/temp.txt", function (err, stdout, stderr) {
+                cp.exec("echo |wc -m /var/ftp/pub/temp.txt", function (err, stdout, stderr) {
                   if (err) {
                     console.error(err);
                   }
