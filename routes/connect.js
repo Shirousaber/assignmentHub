3fec3d28d9cf4a8649a65a2669127ae2fc4dba06
MS-05 <43904074+Shirousaber@users.noreply.github.com>
2020-11-19 19:43:44 +0800

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -154,15 +154,17 @@ router.post('/file_upload', function (req, res) {
                       console.error(err);
                   }
                   else{
+                      var re = '/[\u4E00-\u9FA5]/g'; 
+                      var my_cnt2 = str.match(re).length;
+                      console.log(my_cnt2);
                       var str =data.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,""); 
                       fs.writeFile('/var/ftp/pub/temp.txt', data, function (err){
                         cp.exec("echo |wc -c /var/ftp/pub/temp.txt", function (err, stdout, stderr) {
                           if (err) {
                             console.error(err);
                           }
-//                           my_cnt = stdout.trim().split(" ")[0];
-                          var re = '/[\u4E00-\u9FA5]/g'; 
-                          my_cnt = str.match(re).length; 
+                          my_cnt = stdout.trim().split(" ")[0];
+                          
           //                 var temp_cnt = parseInt(my_cnt);
           //                 my_cnt = ""+temp_cnt;
                           console.log('共计:' + my_cnt);
